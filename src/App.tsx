f28eddd4164bbaa1290334fe81dc80569beacbcd
Максим Стрельников <maksim_stv@mail.ru>
2024-08-13 13:34:48 +0300

    lesson 7 theme and provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom'
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 import { Suspense } from 'react'
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
+
 
 const App = () => {
 
+	const { theme, toggleTheme } = useTheme()
+
 	return (
-		<div >
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Сменить тему</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Загрузка...</div>}>
